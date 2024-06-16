21e5b19713a864b29fce60436adde6310c4051df
RAMYANAGARAJA <142911858+RAMYANAGARAJA@users.noreply.github.com>
2024-06-16 20:57:03 +0530

    Add files via upload

---
+++ ifelse1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter the numbers"<<endl;
+    cin>>a;
+    cin>>b;
+    if(a==b)
+    {
+        cout<<"They are equal"<<endl;
+    }
+    else
+    {
+        cout<<"They are not equal"<<endl;
+    }
+}
