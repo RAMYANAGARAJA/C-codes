0c6f8269aa38e974b03e0909580b14038af8dd48
RAMYANAGARAJA <142911858+RAMYANAGARAJA@users.noreply.github.com>
2024-06-16 18:18:22 +0530

    Add files via upload

---
+++ relationaloperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=20;
+    bool aretheyequal=(a==b);
+    bool isAgreater=(a>b);
+    bool isAlesser=(a<b);
+    cout<<"are they equal"<<aretheyequal<<endl;
+    cout<<"Is A greater"<<isAgreater<<endl;
+    cout<<"Is A lesser"<<isAlesser<<endl;
+}
