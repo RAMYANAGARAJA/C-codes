fd69676457c667a68fac7996a2e359b557da39b4
RAMYANAGARAJA <142911858+RAMYANAGARAJA@users.noreply.github.com>
2024-06-27 13:07:28 +0530

    Add files via upload

---
+++ incrementoperator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int b= a+=3;
+    int c= a-=3;
+    int d= a*=3;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+}
